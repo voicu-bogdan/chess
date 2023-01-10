c29f72a9249093c21ae05c273e6f75ddc9ba43b3
voicu-bogdan <bogdan.iulian.voicu@gmail.com>
2023-01-10 13:46:39 +0200

    Add files via upload

---
+++ Chess/Scenes/Tiles.gd
@@ -0,0 +1,272 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_A1_button_down():
+	print("11")
+
+
+func _on_A2_button_down():
+	print("12")
+
+
+func _on_A3_button_down():
+	print("13")
+
+
+func _on_A4_button_down():
+	print("14")
+
+
+func _on_A5_button_down():
+	print("15")
+
+
+func _on_A6_button_down():
+	print("16")
+
+
+func _on_A7_button_down():
+	print("17")
+
+
+func _on_A8_button_down():
+	print("18")
+
+
+func _on_B1_button_down():
+	print("21")
+
+
+func _on_B2_button_down():
+	print("22")
+
+
+func _on_B4_button_down():
+	print("24")
+
+
+func _on_B3_button_down():
+	print("23")
+
+
+func _on_B5_button_down():
+	print("25")
+
+
+func _on_B6_button_down():
+	print("26")
+
+
+func _on_B7_button_down():
+	print("27")
+
+
+func _on_B8_button_down():
+	print("28")
+
+
+func _on_C1_button_down():
+	print("11")
+
+
+func _on_C2_button_down():
+	print("11")
+
+
+func _on_C3_button_down():
+	print("11")
+
+
+func _on_C4_button_down():
+	print("11")
+
+
+func _on_C5_button_down():
+	print("11")
+
+
+func _on_C6_button_down():
+	print("11")
+
+
+func _on_C7_button_down():
+	print("11")
+
+
+func _on_C8_button_down():
+	print("11")
+
+
+func _on_D1_button_down():
+	print("11")
+
+
+func _on_D2_button_down():
+	print("11")
+
+
+func _on_D3_button_down():
+	print("11")
+
+
+func _on_D4_button_down():
+	print("11")
+
+
+func _on_D5_button_down():
+	print("11")
+
+
+func _on_D6_button_down():
+	print("11")
+
+
+func _on_D7_button_down():
+	print("11")
+
+
+func _on_D8_button_down():
+	print("11")
+
+
+func _on_E1_button_down():
+	print("11")
+
+
+func _on_E2_button_down():
+	print("11")
+
+
+func _on_E3_button_down():
+	print("11")
+
+
+func _on_E4_button_down():
+	print("11")
+
+
+func _on_E5_button_down():
+	print("11")
+
+
+func _on_E6_button_down():
+	print("11")
+
+
+func _on_E7_button_down():
+	print("11")
+
+
+func _on_E8_button_down():
+	print("11")
+
+
+func _on_F1_button_down():
+	print("11")
+
+
+func _on_F2_button_down():
+	print("11")
+
+
+func _on_F3_button_down():
+	print("11")
+
+
+func _on_F4_button_down():
+	print("11")
+
+
+func _on_F5_button_down():
+	print("11")
+
+
+func _on_F6_button_down():
+	print("11")
+
+
+func _on_F7_button_down():
+	print("11")
+
+
+func _on_F8_button_down():
+	print("11")
+
+
+func _on_G1_button_down():
+	print("11")
+
+
+func _on_G2_button_down():
+	print("11")
+
+
+func _on_G3_button_down():
+	print("11")
+
+
+func _on_G4_button_down():
+	print("11")
+
+
+func _on_G5_button_down():
+	print("11")
+
+
+func _on_G6_button_down():
+	print("11")
+
+
+func _on_G7_button_down():
+	print("11")
+
+
+func _on_G8_button_down():
+	print("11")
+
+
+func _on_H1_button_down():
+	print("11")
+
+
+func _on_H2_button_down():
+	print("11")
+
+
+func _on_H3_button_down():
+	print("11")
+
+
+func _on_H4_button_down():
+	print("11")
+
+
+func _on_H5_button_down():
+	print("11")
+
+
+func _on_H6_button_down():
+	print("11")
+
+
+func _on_H7_button_down():
+	print("11")
+
+
+func _on_H8_button_down():
+	print("11")
